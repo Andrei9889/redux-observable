0842b36889f95e82ed4fd6c9ca7c262b7a8f79ed
Andrei Danilkovich <andrei.danilkovich@gmail.com>
2020-09-21 23:08:22 +0300

    rewrite request by internal redux-observable method ajax

--- src/store/epics/fakeDataEpic.js
+++ src/store/epics/fakeDataEpic.js
@@ -1,7 +1,7 @@
-import axios from "axios";
-import { from, of } from "rxjs";
+import { of } from "rxjs";
 import { ofType } from "redux-observable";
 import { startWith, map, catchError, mergeMap } from "rxjs/operators";
+import { ajax } from "rxjs/ajax";
 
 import {
   fakeDataLoading,
@@ -12,11 +12,13 @@ import {
 export const getFakeDataEpic = (action$) =>
   action$.pipe(
     ofType("GET_FAKE_DATA"),
-    mergeMap(action =>
-      from(axios.get(`https://jsonplaceholder.typicode.com/todos/${action.payload}`)).pipe(
-        map((response) => fakeDataRecieved(response.data)),
-        catchError((error) => of(fakeDataErrorRecieved(error))),
-        startWith(fakeDataLoading())
-      )
+    mergeMap((action) =>
+      ajax
+        .getJSON(`https://jsonplaceholder.typicode.com/todos/${action.payload}`)
+        .pipe(
+          map((response) => fakeDataRecieved(response)),
+          catchError((error) => of(fakeDataErrorRecieved(error))),
+          startWith(fakeDataLoading())
+        )
     )
   );
